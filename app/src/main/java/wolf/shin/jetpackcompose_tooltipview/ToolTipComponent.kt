79373be17d25e8e2dde7b6e759bf5a95f44d6931
shindonghwi <ehdgnl8940@naver.com>
2022-04-30 15:39:13 +0900

    feat: 툴팁 컴포넌트 추가

---
+++ app/src/main/java/wolf/shin/jetpackcompose_tooltipview/ToolTipComponent.kt
@@ -0,0 +1,53 @@
+package wolf.shin.jetpackcompose_tooltipview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import org.jetbrains.annotations.NotNull
+
+data class ToolTipComponent(
+    val style: ToolTipStyle,
+    val direction: ToolTipDirection,
+    val backgroundColor: Color,
+    val title: ToolTipText? = null,
+    val content: ToolTipText,
+    val icon: Triple<Int, Color, () -> Unit>? = null // 아이콘 drawable
+)
+
+fun getToolTipComponent(
+    style: ToolTipStyle,
+    direction: ToolTipDirection,
+    backgroundColor: Color,
+    title: ToolTipText? = null,
+    content: ToolTipText,
+    icon: Triple<Int, Color, () -> Unit>? = null // 아이콘 drawable
+): ToolTipComponent {
+    return ToolTipComponent(
+        style = style,
+        direction = direction,
+        backgroundColor = backgroundColor,
+        title = title,
+        content = content,
+        icon = icon
+    )
+}
+
+enum class ToolTipStyle(val style: Int) {
+    DEFAULT(0),
+    TITLE(1),
+}
+
+data class ToolTipText(
+    val text: String,
+    val textColor: Color = Color.White,
+    @NotNull val textStyle: TextStyle
+)
+
+enum class ToolTipDirection {
+    CENTER_TOP,
+    CENTER_BOTTOM,
+    RIGHT_TOP,
+    RIGHT_BOTTOM,
+    LEFT_TOP,
+    LEFT_BOTTOM,
+    RIGHT_CENTER,
+    LEFT_CENTER,
+}
