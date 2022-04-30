3dc31426a62841bd76e9dd59a4acb8f0b573554a
shindonghwi <ehdgnl8940@naver.com>
2022-04-30 15:47:28 +0900

    fix: 기본 툴팁 UI 제공화면 추가

--- app/src/main/java/wolf/shin/jetpackcompose_tooltipview/MainActivity.kt
+++ app/src/main/java/wolf/shin/jetpackcompose_tooltipview/MainActivity.kt
@@ -5,11 +5,15 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import wolf.shin.jetpackcompose_tooltipview.ui.theme.JetpackCompose_ToolTipViewTheme
+import java.security.SecureRandom
 import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
@@ -29,36 +33,44 @@ class MainActivity : ComponentActivity() {
                     ToolTipDirection.RIGHT_TOP,
                 )
 
-                Column(modifier = Modifier.fillMaxSize()) {
+                Column(modifier = Modifier
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState())) {
 
                     directionList.forEachIndexed { index, direction ->
-                        Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceAround) {
+
+                        Row(modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 12.dp), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceAround) {
 
                             ToolTip(
                                 component = getToolTipComponent(
-                                    style = ToolTipStyle.TITLE,
+                                    style = if (SecureRandom().nextInt(2) == 0) ToolTipStyle.TITLE else ToolTipStyle.DEFAULT,
                                     direction = direction,
-                                    backgroundColor = Color.Black,
+                                    backgroundColor = Color.random(),
                                     title = ToolTipText("Title${index}", Color.White, MaterialTheme.typography.subtitle1),
                                     content = ToolTipText("Content${index}", Color.White, MaterialTheme.typography.body2),
-                                    icon = ToolTipIcon(img = R.drawable.ic_info, tint = Color.White) { Toast.makeText(this@MainActivity, "asdasd", Toast.LENGTH_LONG).show() }
+                                    icon = if (SecureRandom().nextInt(2) == 0) ToolTipIcon(img = R.drawable.ic_info, tint = Color.White) {
+                                        Toast.makeText(this@MainActivity, "Icon Click", Toast.LENGTH_LONG).show()
+                                    } else null
                                 )
                             )
 
                             ToolTip(
                                 component = getToolTipComponent(
-                                    style = ToolTipStyle.TITLE,
+                                    style = if (SecureRandom().nextInt(2) == 0) ToolTipStyle.TITLE else ToolTipStyle.DEFAULT,
                                     direction = direction,
-                                    backgroundColor = Color.Black,
+                                    backgroundColor = Color.random(),
                                     title = ToolTipText("Title${index}", Color.White, MaterialTheme.typography.subtitle1),
                                     content = ToolTipText("Content${index}", Color.White, MaterialTheme.typography.body2),
-                                    icon = ToolTipIcon(img = R.drawable.ic_info, tint = Color.White) { Toast.makeText(this@MainActivity, "asdasd", Toast.LENGTH_LONG).show() }
+                                    icon = if (SecureRandom().nextInt(2) == 0) ToolTipIcon(img = R.drawable.ic_info, tint = Color.White) {
+                                        Toast.makeText(this@MainActivity, "Icon Click", Toast.LENGTH_LONG).show()
+                                    } else null
                                 )
                             )
                         }
                     }
                 }
-
             }
         }
     }
